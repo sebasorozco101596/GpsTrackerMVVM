ba238f60c4991e377048719ff79b1a9b2fff5e1b
sebasorozco101596 <sebasorozcob@gmail.com>
2021-04-27 00:51:10 -0400

    Adding the Room Database and injection dependencies with Dagger-Hilt

---
+++ app/src/main/java/com/sebasorozcob/www/gpstrackermvvm/BaseApplication.kt
@@ -0,0 +1,11 @@
+package com.sebasorozcob.www.gpstrackermvvm
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication: Application() {
+
+
+
+}
---
+++ app/src/main/java/com/sebasorozcob/www/gpstrackermvvm/db/RunningDatabase.kt
@@ -0,0 +1,17 @@
+package com.sebasorozcob.www.gpstrackermvvm.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+@Database(
+    entities = [Run::class],
+    version = 1,
+    exportSchema = false
+)
+@TypeConverters(Converters::class)
+abstract class RunningDatabase : RoomDatabase() {
+
+    abstract fun getRunDao(): RunDAO
+
+}
