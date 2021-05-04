612b1a0003399b39ce21ae7a982e53f7c1e4ad8e
sebasorozco101596 <sebasorozcob@gmail.com>
2021-05-03 23:56:43 -0400

    Adding the foreground service and the notification

--- app/src/main/java/com/sebasorozcob/www/gpstrackermvvm/ui/activities/MainActivity.kt
+++ app/src/main/java/com/sebasorozcob/www/gpstrackermvvm/ui/activities/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.sebasorozcob.www.gpstrackermvvm.ui.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -7,6 +8,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.sebasorozcob.www.gpstrackermvvm.R
 import com.sebasorozcob.www.gpstrackermvvm.databinding.ActivityMainBinding
+import com.sebasorozcob.www.gpstrackermvvm.util.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import dagger.hilt.android.AndroidEntryPoint
 
 private const val TAG = "MainActivity"
@@ -22,8 +24,10 @@ class MainActivity : AppCompatActivity() {
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        navigateToTrackingFragmentIfNeeded(intent)
+
         setSupportActionBar(binding.toolbar)
-        with(binding){
+        with(binding) {
             bottomNavigationView.setupWithNavController(
                 flFragment.findViewById<View>(
                     R.id.navHostFragment
@@ -39,6 +43,18 @@ class MainActivity : AppCompatActivity() {
                     else -> bottomNavigationView.visibility = View.GONE
                 }
             }
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        navigateToTrackingFragmentIfNeeded(intent)
+    }
+
+    private fun navigateToTrackingFragmentIfNeeded(intent: Intent?) {
+        if (intent?.action == ACTION_SHOW_TRACKING_FRAGMENT) {
+            binding.flFragment.findViewById<View>(R.id.navHostFragment).findNavController()
+                .navigate(R.id.action_global_trackingFragment)
         }
     }
 }
--- app/src/main/java/com/sebasorozcob/www/gpstrackermvvm/util/Constants.kt
+++ app/src/main/java/com/sebasorozcob/www/gpstrackermvvm/util/Constants.kt
@@ -12,5 +12,11 @@ object Constants {
     const val ACTION_START_OR_RESUME_SERVICE = "ACTION_START_OR_RESUME_SERVICE"
     const val ACTION_PAUSE_SERVICE = "ACTION_PAUSE_SERVICE"
     const val ACTION_STOP_SERVICE = "ACTION_STOP_SERVICE"
+    const val ACTION_SHOW_TRACKING_FRAGMENT = "ACTION_SHOW_TRACKING_FRAGMENT"
+
+    // Notifications
+    const val NOTIFICATION_CHANNEL_ID = "tracking_channel"
+    const val NOTIFICATION_CHANNEL_NAME = "Tracking"
+    const val NOTIFICATION_ID = 1 // If i put 0 as ID I gonna receive a error
 
 }
