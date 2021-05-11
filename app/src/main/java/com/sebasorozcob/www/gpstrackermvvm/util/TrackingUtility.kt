67511e237f09f22e46fb198fb44cfa6a916ef07c
sebasorozco101596 <sebasorozcob@gmail.com>
2021-05-10 23:37:01 -0400

    Adding the stop run and save it in the Room Database

--- app/src/main/java/com/sebasorozcob/www/gpstrackermvvm/util/TrackingUtility.kt
+++ app/src/main/java/com/sebasorozcob/www/gpstrackermvvm/util/TrackingUtility.kt
@@ -2,9 +2,10 @@ package com.sebasorozcob.www.gpstrackermvvm.util
 
 import android.Manifest
 import android.content.Context
+import android.location.Location
 import android.os.Build
+import com.sebasorozcob.www.gpstrackermvvm.services.Polyline
 import pub.devrel.easypermissions.EasyPermissions
-import java.sql.Time
 import java.util.concurrent.TimeUnit
 
 object TrackingUtility {
@@ -25,6 +26,27 @@ object TrackingUtility {
             )
         }
 
+    fun calculatePolylineLength(polyline: Polyline): Float {
+        var distance = 0f
+        for (i in 0..polyline.size - 2) {
+            val pos1 = polyline[i]
+            val pos2 = polyline[i + 1]
+
+            val result = FloatArray(1)
+
+            Location.distanceBetween(
+                pos1.latitude,
+                pos1.longitude,
+                pos2.latitude,
+                pos2.longitude,
+                result
+            )
+            distance += result[0]
+        }
+
+        return distance
+    }
+
     fun getFormattedStopWatchTime(ms: Long, includeMillis: Boolean = false): String {
         var milliseconds = ms
         val hours = TimeUnit.MILLISECONDS.toHours(milliseconds)
@@ -34,17 +56,17 @@ object TrackingUtility {
         val seconds = TimeUnit.MILLISECONDS.toSeconds(milliseconds)
 
         if (!includeMillis) {
-            return "${if(hours < 10) "0" else ""}$hours:" +
-                    "${if(minutes < 10) "0" else ""}$minutes:" +
-                    "${if(seconds < 10) "0" else ""}$seconds:"
+            return "${if (hours < 10) "0" else ""}$hours:" +
+                    "${if (minutes < 10) "0" else ""}$minutes:" +
+                    "${if (seconds < 10) "0" else ""}$seconds:"
         }
 
         milliseconds -= TimeUnit.SECONDS.toMillis(seconds)
         milliseconds /= 10
 
-        return "${if(hours < 10) "0" else ""}$hours:" +
-                "${if(minutes < 10) "0" else ""}$minutes:" +
-                "${if(seconds < 10) "0" else ""}$seconds:" +
-                "${if(milliseconds < 10) "0" else ""}$milliseconds"
+        return "${if (hours < 10) "0" else ""}$hours:" +
+                "${if (minutes < 10) "0" else ""}$minutes:" +
+                "${if (seconds < 10) "0" else ""}$seconds:" +
+                "${if (milliseconds < 10) "0" else ""}$milliseconds"
     }
 }
