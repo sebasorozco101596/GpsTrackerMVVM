42368fd90a814c97aadba82b00e551aebe5f3e63
sebasorozco101596 <sebasorozcob@gmail.com>
2021-05-13 00:11:43 -0400

    Adding the information of the StatisticsFragment.kt and StatisticsViewModel.kt for get all the information of the user in the app, also I added the SettingsFragment.kt with the implementation for edit the name and weight of the user.

--- app/src/main/java/com/sebasorozcob/www/gpstrackermvvm/viewmodels/StatisticsViewModel.kt
+++ app/src/main/java/com/sebasorozcob/www/gpstrackermvvm/viewmodels/StatisticsViewModel.kt
@@ -11,4 +11,12 @@ class StatisticsViewModel @Inject constructor(
     private val repository: MainRepository,
     application: Application
 ) : AndroidViewModel(application) {
+
+    val totalTimeRun = repository.getTotalTimeInMillis()
+    val totalDistance = repository.getTotalDistance()
+    val totalCaloriesBurned = repository.getCaloriesBurned()
+    val totalAvgSpeed = repository.getTotalAvgSpeed()
+
+    val runsSortedByDate = repository.getAllRunsSortedByDate()
+
 }
